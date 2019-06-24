6d3713cf1aa4c251fbf4f12ed78b45b62588cdfd
liz <93873637@qq.com>
2019-06-24 14:56:39 +0800

    update 190624.145620

--- update_to_github.sh
+++ update_to_github.sh
@@ -1,4 +1,19 @@
-git status
-git add .
-git commit -m "update daily"
-git push git@github.com:93873637/Android.git
+#!/bin/bash 
+
+echo "update_to_github..."
+
+TIME_FLAG=`date +"%y%m%d.%H%M%S"`
+
+run_git_cmd(){
+    #echo "run_git_cmd..."
+    echo ""
+    echo "# $*"
+    $*
+    echo ""
+}
+
+run_git_cmd "git status"
+run_git_cmd "git add ."
+run_git_cmd "git commit -m \"update ${TIME_FLAG}\""
+run_git_cmd "git push git@github.com:93873637/Android.git"
+run_git_cmd "git status"
