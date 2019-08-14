6b1e992d00844020f7307026698d2f02c9c3f969
liz <93873637@qq.com>
2019-08-15 00:48:02 +0800

    update_190815.004801

--- AndroidUtils/AndroidUtilsLib/src/main/java/com/liz/androidutils/FileUtils.java
+++ AndroidUtils/AndroidUtilsLib/src/main/java/com/liz/androidutils/FileUtils.java
@@ -1,10 +1,13 @@
 package com.liz.androidutils;
 
+import java.io.File;
+
 /**
  * FileUtils:
  * Created by liz on 2019/1/14.
  */
 
+@SuppressWarnings("unused, WeakerAccess")
 public class FileUtils {
     /**
      * @return get file extension name from file absolute path
@@ -26,5 +29,15 @@ public class FileUtils {
             return "";
         }
         return fileAbsolute.substring(0, index);
+    }
+
+    public static void removeFile(String fileName) {
+        File f = new File(fileName);
+        if (f.exists()) {
+            //System.out.println("image file " + fileName + " already exist, delete it");
+            if (!f.delete()) {
+                System.out.println("Delete file " + fileName + " failed.");
+            }
+        }
     }
 }
--- AndroidUtils/AndroidUtilsTest/src/main/java/com/liz/androidutilstest/MainActivity.java
+++ AndroidUtils/AndroidUtilsTest/src/main/java/com/liz/androidutilstest/MainActivity.java
@@ -11,6 +11,7 @@ import com.liz.androidutils.ComUtils;
 import com.liz.androidutils.ImageUtils;
 import com.liz.androidutils.LogUtils;
 import com.liz.androidutils.SysUtils;
+import com.liz.androidutils.FileUtils;
 
 import java.nio.ByteBuffer;
 
@@ -46,15 +47,15 @@ public class MainActivity extends AppCompatActivity {
     //Test Functions
 
     public void test() {
-        //test_image();
-        test_SysUtils();
+        test_image();
+        //test_SysUtils();
     }
 
     public void test_SysUtils() {
         LogUtils.d("test_SysUtils: appVersion = " + SysUtils.getAppVersion(MainActivity.this));
     }
 
-    public void test_image() {
+    public void test_image1() {
         String dataFileName = "/sdcard/data.txt";
         String imageFileName = dataFileName + ".jpg";
 
@@ -93,18 +94,105 @@ public class MainActivity extends AppCompatActivity {
 
         //bb.flip();
         ImageUtils.saveByteBuffer2JPGFile(bb, imageFileName, imageWidth, imageHeight, pixelPadding);
+    }
+
+    public void test_image() {
+        String dataFileName = "/sdcard/data.txt";
+
+        String imageFileName = dataFileName + ".jpg";
+        String imageFileName2 = dataFileName + "2.jpg";
+        String imageFileName3 = dataFileName + "3.jpg";
+
+        FileUtils.removeFile(imageFileName);
+        FileUtils.removeFile(imageFileName2);
+        FileUtils.removeFile(imageFileName3);
+
+        ByteBuffer byteBuffer = ComUtils.readByteBufferFromFile(dataFileName);
+        if (byteBuffer == null) {
+            System.out.println("read failed");
+            return;
+        }
+        else {
+            System.out.println("read ok, buffer size = " + byteBuffer.capacity());
+        }
+
+        //RGBA_8888
+
+        ////////////////////////////////////////////////////////////////////
+        //image Size: 1440 x 2392
+        //total Size: 1472 x 2392
+        //pixelStride = 4
+        //rowStride = 5888 (64*23)
+        //rowPadding = 128
+        //buffer capacity = 14084096 = 5888*2392 = (1440 + 32) * 4 * 2392
+        ////////////////////////////////////////////////////////////////////
+
+        int imageWidth = 1440;
+        int imageHeight = 2392;
+
+        int pixelStride = 4;
+        int rowPadding = 128;
+
+        int pixelPadding = rowPadding / pixelStride;  //32
+
+        int totalWidth = imageWidth + pixelPadding;  //1472
+        int totalHeight = imageHeight;
+
+        int rowStride = totalWidth * pixelStride;  //5888
+
+        ImageUtils.saveByteBuffer2JPGFile(byteBuffer, imageFileName, imageWidth, imageHeight, pixelPadding);
 
 
+        int scale = 4;
+        int imageHeight2 = totalHeight / scale;
+        ByteBuffer byteBuffer2 = ByteBuffer.allocate(rowStride * imageHeight2);
+        byteBuffer.flip();
+        byteBuffer2.flip();
+        LogUtils.d("###@: imageHeight2=" + imageHeight2);
+        LogUtils.d("###@: bb.position/limit/capacity=" + byteBuffer.position() + "/" + byteBuffer.limit() + "/" + byteBuffer.capacity()
+                + ": byteBuffer2.position/limit/capacity=" + byteBuffer2.position() + "/" + byteBuffer2.limit() + "/" + byteBuffer2.capacity());
 
-//
-//        bb.flip();
-//        bb.position(0);
-//        bb.limit(rowStride);
-//        ByteBuffer slice = bb.slice();
-//        System.out.println("get slice, size = " + slice.capacity());
+        for (int i = 0; i < imageHeight2; i ++) {
+            byteBuffer.limit((i * scale + 1) * rowStride);
+            byteBuffer.position(i * scale * rowStride);
+            byteBuffer2.limit((i + 1) * rowStride);
+            byteBuffer2.position(i * rowStride);
+            //LogUtils.d("###@: i=" + i + ": bb.position/limit/capacity=" + bb.position() + "/" + bb.limit() + "/" + bb.capacity()
+            //        + ": byteBuffer2.position/limit/capacity=" + byteBuffer2.position() + "/" + byteBuffer2.limit() + "/" + byteBuffer2.capacity());
+            byteBuffer2.put(byteBuffer.slice());
+        }
 
+        ImageUtils.saveByteBuffer2JPGFile(byteBuffer2, imageFileName2, imageWidth, imageHeight2, pixelPadding);
 
+        int imageWidth2 = imageWidth / scale;
+        int imageBytes = imageWidth2 * imageHeight2 * (pixelStride);
+        LogUtils.d("###@: imageWidth2=" + imageWidth2);
+        LogUtils.d("###@: imageHeight2=" + imageHeight2);
+        LogUtils.d("###@: imageBytes=" + imageBytes);
 
+        byte[] imageArr = new byte[imageBytes];
+        int offset;
+        int offset2;
+        for (int i = 0; i < imageHeight2; i ++) {
+            offset = i * rowStride;
+            byteBuffer2.limit(offset + rowStride);
+            byteBuffer2.position(offset);
+            byte[] arr = ComUtils.byteBuf2Arr(byteBuffer2.slice());
+            LogUtils.d("###@: arr=" + arr.length);
+            offset2 = i * imageWidth2 * pixelStride;
+            LogUtils.d("###@: offset2=" + offset2);
+            for (int j = 0; j < imageWidth2; j++) {
+                int imgOff = offset2 + j * 4;
+                int arrOff = j*4*4;
+                imageArr[imgOff] = arr[arrOff];
+                imageArr[imgOff+1] = arr[arrOff+1];
+                imageArr[imgOff+2] = arr[arrOff+2];
+                imageArr[imgOff+3] = arr[arrOff+3];
+            }
+        }
 
+        ByteBuffer byteBuffer3 = ComUtils.byteArr2Buf(imageArr);
+        LogUtils.d("###@: byteBuffer3.position/limit/capacity=" + byteBuffer3.position() + "/" + byteBuffer3.limit() + "/" + byteBuffer3.capacity());
+        ImageUtils.saveByteBuffer2JPGFile(byteBuffer3, imageFileName3, imageWidth2, imageHeight2, 0);
     }
 }
