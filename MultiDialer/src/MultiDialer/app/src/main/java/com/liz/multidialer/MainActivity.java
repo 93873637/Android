4344a37a690e72a3923cad75b97daed34720aaef
liz <93873637@qq.com>
2019-10-18 09:56:07 +0800

    update_191018.095606

--- MultiDialer/src/MultiDialer/app/src/main/java/com/liz/multidialer/MainActivity.java
+++ MultiDialer/src/MultiDialer/app/src/main/java/com/liz/multidialer/MainActivity.java
@@ -4,15 +4,33 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.graphics.PixelFormat;
+import android.hardware.display.DisplayManager;
+import android.hardware.display.VirtualDisplay;
+import android.media.Image;
+import android.media.ImageReader;
+import android.media.projection.MediaProjection;
+import android.media.projection.MediaProjectionManager;
 import android.os.Bundle;
 import android.os.Handler;
+import android.text.method.ScrollingMovementMethod;
+import android.util.DisplayMetrics;
 import android.view.View;
+import android.view.WindowManager;
+import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.liz.androidutils.ImageUtils;
+import com.liz.androidutils.LogUtils;
 import com.liz.androidutils.TelUtils;
 import com.liz.androidutils.FileUtils;
+import com.liz.androidutils.TimeUtils;
 
 import java.util.ArrayList;
 
@@ -20,7 +38,8 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String TEL_LIST_FILE_NAME = "/sdcard/tellist.txt";
 
-    private TextView mTextInfo;
+    private TextView mTextProgress;
+    private ScrollView mScrollProgress;
     private TextView mTextTelNumber;
 
     private int mTelIndex = 0;
@@ -34,6 +53,123 @@ public class MainActivity extends AppCompatActivity {
             Manifest.permission.CALL_PHONE
     };
 
+    private static boolean mCaptureOnce;
+
+    private static final int REQUEST_MEDIA_PROJECTION = 1;
+
+    private int mResultCode;
+    private Intent mResultData;
+
+    private MediaProjection mMediaProjection;
+    private VirtualDisplay mVirtualDisplay;
+    private MediaProjectionManager mMediaProjectionManager;
+    private static ImageReader mImageReader = null;
+
+    private int mWindowWidth = 0;
+    private int mWindowHeight = 0;
+    private int mScreenDensity = 0;
+
+    private ImageReader.OnImageAvailableListener mOnImageAvailableListener
+            = new ImageReader.OnImageAvailableListener() {
+        @Override
+        public void onImageAvailable(ImageReader reader) {
+            //showProgress("onImageAvailable");
+            Image img = reader.acquireNextImage();
+            if (mCaptureOnce) {
+                onCaptureOnce(img);
+                mCaptureOnce = false;
+            }
+            img.close();  //NOTE: you must close the image to get next
+        }
+    };
+
+    private void onCaptureOnce(Image img) {
+        //save image to jpg file
+        String jpgFileName = "/sdcard/multidialer/" + getCurrentTelNumber() + ".jpg";;
+        int ret = ImageUtils.saveImage2JPGFile(img, jpgFileName);
+        if (ret < 0) {
+            showProgress("save screen image to " + jpgFileName + " failed with error " + ret);
+        } else {
+            showProgress("screen image saved to " + jpgFileName);
+        }
+    }
+
+    private void startScreenCapture() {
+        showProgress("startScreenCapture");
+
+        if (mMediaProjection == null) {
+            mMediaProjection = mMediaProjectionManager.getMediaProjection(mResultCode, mResultData);
+        }
+        else {
+            showProgress("ERROR: startScreenCapture: mMediaProjection already exists");
+        }
+
+        if (mImageReader == null) {
+            //
+            //NOTE: only PixelFormat.RGBA_8888(1) is supported for my A2H
+            //java.lang.UnsupportedOperationException: The producer output buffer format 0x1 doesn't match the ImageReader's configured buffer format 0x3.
+            //
+            mImageReader = ImageReader.newInstance(mWindowWidth, mWindowHeight, PixelFormat.RGBA_8888, 2); //ImageFormat.RGB_565
+            mImageReader.setOnImageAvailableListener(mOnImageAvailableListener, null);
+        }
+        else {
+            showProgress("ERROR: startScreenCapture: mImageReader already exists");
+        }
+
+        if (mVirtualDisplay == null) {
+            mVirtualDisplay = mMediaProjection.createVirtualDisplay("screen-mirror",
+                    mWindowWidth, mWindowHeight, mScreenDensity, DisplayManager.VIRTUAL_DISPLAY_FLAG_AUTO_MIRROR,
+                    mImageReader.getSurface(),
+                    null, null);
+        }
+        else {
+            showProgress("ERROR: startScreenCapture: mVirtualDisplay already exists");
+        }
+    }
+
+    private void stopScreenCapture() {
+        showProgress("stopScreenCapture");
+
+        if (mVirtualDisplay == null) {
+            showProgress("ERROR: stopScreenCapture: mVirtualDisplay null");
+        }
+        else {
+            mVirtualDisplay.release();
+            mVirtualDisplay = null;
+        }
+
+        if (mImageReader == null) {
+            showProgress("ERROR: stopScreenCapture: mImageReader null");
+        }
+        else {
+            mImageReader.setOnImageAvailableListener(null, null);
+            mImageReader.close();
+            mImageReader = null;
+        }
+
+        if (mMediaProjection == null) {
+            showProgress("ERROR: stopScreenCapture: mMediaProjection null");
+        }
+        else {
+            mMediaProjection.stop();
+            mMediaProjection = null;
+        }
+    }
+
+    public static boolean isCaptureOn() {
+        return mImageReader != null;
+    }
+
+    public static void captureOnce() {
+        LogUtils.d("captureOnce");
+        if (isCaptureOn()) {
+            mCaptureOnce = true;
+        }
+        else {
+            LogUtils.d("captureOnce: no image reader to capture");
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,13 +179,33 @@ public class MainActivity extends AppCompatActivity {
 
         mTelList = FileUtils.readTxtFileLines(TEL_LIST_FILE_NAME);
 
-        mTextInfo = findViewById(R.id.text_dial_info);
         mTextTelNumber = findViewById(R.id.text_tel_list_info);
         mTextTelNumber.setText("电话号码数量(" + TEL_LIST_FILE_NAME + "): " + mTelList.size());
 
+        mTextProgress = findViewById(R.id.text_dial_info);
+        mTextProgress.setText("");
+        mScrollProgress = findViewById(R.id.scrollInfo);
+        mScrollProgress.setBackgroundColor(Color.LTGRAY);
+        mTextProgress.setMovementMethod(ScrollingMovementMethod.getInstance());
+
+        WindowManager windowManager = (WindowManager)this.getSystemService(Context.WINDOW_SERVICE);
+        mWindowWidth = windowManager.getDefaultDisplay().getWidth();
+        mWindowHeight = windowManager.getDefaultDisplay().getHeight();
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        windowManager.getDefaultDisplay().getMetrics(displayMetrics);
+        mScreenDensity = displayMetrics.densityDpi;
+
+        DisplayMetrics metrics = new DisplayMetrics();
+        this.getWindowManager().getDefaultDisplay().getMetrics(metrics);
+        mScreenDensity = metrics.densityDpi;
+        mMediaProjectionManager = (MediaProjectionManager)this.getSystemService(Context.MEDIA_PROJECTION_SERVICE);
+        startActivityForResult(
+                mMediaProjectionManager.createScreenCaptureIntent(),
+                REQUEST_MEDIA_PROJECTION);
+
         //##@: test
 //        for (int i=0; i<100; i++) {
-//            showMsg("#" + i + ": Start Call: 12345678901, OKssssssssssssssssssssssssssssssssssssssssssssssssssssssssss");
+//            showProgress("#" + i + ": Start Call: 12345678901, OKssssssssssssssssssssssssssssssssssssssssssssssssssssssssss");
 //        }
 
         findViewById(R.id.btn_start_call).setOnClickListener(new View.OnClickListener() {
@@ -58,6 +214,31 @@ public class MainActivity extends AppCompatActivity {
                 startCallOnList();
             }
         });
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_MEDIA_PROJECTION) {
+            if (resultCode != Activity.RESULT_OK) {
+                String tip = "ERROR: resultCode = "+resultCode+", NOT OK";
+                showProgress(tip);
+                Toast.makeText(MainActivity.this, tip, Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            Activity activity = MainActivity.this;
+            if (activity == null) {
+                showProgress("ERROR: activity is null");
+                return;
+            }
+
+            showProgress("onActivityResult OK");
+            mResultCode = resultCode;
+            mResultData = data;
+            startScreenCapture();
+        }
+
+        super.onActivityResult(requestCode, resultCode, data);
     }
 
     protected void checkPermissions() {
@@ -78,39 +259,64 @@ public class MainActivity extends AppCompatActivity {
 
     private void startCallOnList() {
         if (mTelList == null) {
-            showMsg("startCallOnList: list null");
+            showProgress("startCallOnList: list null");
             return;
         }
         mTelIndex = 0;
-        startCallOnNum(mTelIndex);
+        startCallOnNum();
     }
 
-    private void startCallOnNum(int idx) {
-        if (idx >= mTelList.size()) {
-            showMsg("***Call Finished.");
-            return;
-        }
-        String strTel = mTelList.get(idx);
+    private String getCurrentTelNumber() {
+        return mTelList.get(mTelIndex);
+    }
+
+    private void startCallOnNum() {
+        String strTel = getCurrentTelNumber();
         try {
             String ret = TelUtils.startCall(MainActivity.this, strTel);
-            showMsg("#" + (idx+1) + ": Start Call, Tel = " + strTel + ", " + ret);
+            showProgress("#" + (mTelIndex+1) + ": Start Call, Tel = " + strTel + ", " + ret);
 
             new Handler().postDelayed(new Runnable() {
                 public void run() {
+                    //before end call, we should take screen picture
+                    captureOnce();
+
+                    //end current call
                     String retEndCall = TelUtils.endCall(MainActivity.this);
-                    showMsg("End Call: " + retEndCall);
-                    mTelIndex++;
-                    startCallOnNum(mTelIndex);
+                    showProgress("End Call: " + retEndCall);
+
+                    //start next call
+                    mTelIndex ++;
+                    if (mTelIndex < mTelList.size()) {
+                        startCallOnNum(mTelIndex);
+                    }
+                    else {
+
+                    }
                 }
             }, 2 * 1000);  // 延迟n秒后自动挂断电话
         } catch (Exception e) {
             Toast.makeText(MainActivity.this, "OnClick Exception: " + e.toString(), Toast.LENGTH_SHORT).show();
             e.printStackTrace();
-            showMsg("startCallOnNum Exception: " + e.toString());
+            showProgress("startCallOnNum Exception: " + e.toString());
         }
     }
 
-    private void showMsg(String msg) {
-        mTextInfo.append(msg + "\n");
+    private void showProgress(final String msg) {
+        Activity activity = MainActivity.this;
+        if (activity != null) {
+            activity.runOnUiThread(new Runnable() {
+                public void run() {
+                    String logMsg = TimeUtils.getLogTime() + " - " + msg;
+                    mTextProgress.append(logMsg + "\n");
+                    mScrollProgress.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            mScrollProgress.smoothScrollTo(0, mTextProgress.getBottom());
+                        }
+                    });
+                }
+            });
+        }
     }
 }
